88e957fa6258dd29bbe8dda3515c5b8a8ad2e148
Ruba <rsharif24@hotmail.com>
2017-06-11 18:09:32 -0500

    fixed data-animate and data-still states

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,10 +56,10 @@ $(document).ready(function() {
         var state = $(this).attr('data-state');
 
         if (state === 'still') {
-            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('src', $(this).attr('data-animate'));
             $(this).attr('data-state', 'animate');
         } else {
-            $(this).attr('src', $(this).data('still'));
+            $(this).attr('src', $(this).attr('data-still'));
             $(this).attr('data-state', 'still');
         }
     });
